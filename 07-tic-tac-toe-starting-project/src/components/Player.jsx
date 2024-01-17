9ebe421b7e111f3dca89751af5ca134a1592e002
melissalf <melissalantiguafermin@gmail.com>
2024-01-17 15:21:16 -0400

    changes for 07-tic-tac-toe

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function Player({ initialName, symbol, isActive, onChangeName }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let playerPresentation = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    playerPresentation = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {playerPresentation}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
+
+export default Player;
